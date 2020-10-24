8883eb36c65e159e9c310c29ab8450acfcf5d4c3
Gabriel Fontes <g.fontes@usp.br>
2020-10-24 01:40:25 -0300

    Improve package scripts

--- ofpackage.sh
+++ ofpackage.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/bash
 version=$(cat version)
+./manifest.sh | jq > curseforge/manifest.json
+
+mv curseforge/overrides/mods .tmp_mods
 mv .optifine/{mods,shaderpacks,optionsshaders.txt} curseforge/overrides/
 
-./manifest.sh | jq > curseforge/manifest.json
 rm releaze.zip
 cd curseforge
 zip release.zip overrides manifest.json -r
 cd ../
+
 mv curseforge/release.zip modpack-da-informacao-$version-optifine.zip
 
 mv curseforge/overrides/{mods,shaderpacks,optionsshaders.txt} .optifine
+mv .tmp_mods curseforge/overrides/mods
--- package.sh
+++ package.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 version=$(cat version)
 ./manifest.sh | jq > curseforge/manifest.json
+
+mv curseforge/overrides/mods .tmp_mods
+
 rm releaze.zip
 cd curseforge
 zip release.zip overrides manifest.json -r
 cd ../
+
 mv curseforge/release.zip modpack-da-informacao-$version.zip
+mv .tmp_mods curseforge/overrides/mods
