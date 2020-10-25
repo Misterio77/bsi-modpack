3fd73553c28ce09f62b5b589982d26648453b200
Gabriel Fontes <g.fontes@usp.br>
2020-10-25 00:31:02 -0300

    Fixed unwanted unify on quartz, disable unify on inv open/close (so you can use copper blocks, for instance). Disable automatic nether portals on AA. Disable darksouls mode on quark, enabled backpack crafting. Added tinker tools leveling.

--- copy-client.sh
+++ copy-client.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 mkdir curseforge/overrides/{config,mods} -p
-cp ~/.local/share/multimc/instances/modpack*/minecraft/config/* curseforge/overrides/config -r
-cp ~/.local/share/multimc/instances/modpack*/minecraft/mods/* curseforge/overrides/mods -r
+cp ~/.local/share/multimc/instances/*odpack*/minecraft/config/* curseforge/overrides/config -r
+cp ~/.local/share/multimc/instances/*odpack*/minecraft/mods/* curseforge/overrides/mods -r
