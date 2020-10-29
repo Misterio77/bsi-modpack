5dbe5ec41248674226d857151ce50e6f0bf13196
Gabriel Fontes <g.fontes@usp.br>
2020-10-29 20:56:22 -0300

    Fix numina's invasive fov 'fix'

--- copy-client.sh
+++ copy-client.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+rm curseforge/overrides/{config,mods} -r
 mkdir curseforge/overrides/{config,mods} -p
 cp ~/.local/share/multimc/instances/*odpack*/minecraft/config/* curseforge/overrides/config -r
 cp ~/.local/share/multimc/instances/*odpack*/minecraft/mods/* curseforge/overrides/mods -r
