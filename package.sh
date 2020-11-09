179671635ce286b674b2ec1542f7089016094be5
Gabriel Fontes <g.fontes@usp.br>
2020-11-09 18:24:39 -0300

    Added incorporeal, added more books to akashic tome, reworked package script

--- package.sh
+++ package.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/bash
 version=$(cat version)
 ./manifest.sh | jq > curseforge/manifest.json
-
+rm *.zip
 mv curseforge/overrides/mods .tmp_mods
 
-rm releaze.zip
 cd curseforge
 zip release.zip overrides manifest.json -r
 cd ../
 
 mv curseforge/release.zip modpack-da-informacao-$version.zip
 
+# Package optifine version
+# Move optifine files
+mv .optifine/{mods,shaderpacks,optionsshaders.txt} curseforge/overrides/
+
+cd curseforge
+zip release.zip overrides manifest.json -r
+cd ../
+
+mv curseforge/release.zip modpack-da-informacao-$version-optifine.zip
+
+mv curseforge/overrides/{mods,shaderpacks,optionsshaders.txt} .optifine
 mv .tmp_mods curseforge/overrides/mods
